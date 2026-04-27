e4bab9fe6bfbcfb36975a2acbda29984f328391f
Jung TaeSeung <mike415415@naver.com>
2026-04-28 02:21:30 +0900

    feat : github oauth parameter 수정

--- frontend/app/type/GithubOAuth.ts
+++ frontend/app/type/GithubOAuth.ts
@@ -4,7 +4,7 @@
 export interface GithubOAuthParams {
   client_id: string;
   redirect_uri: string;
-  scope?: string;
+  scope?: string[];
   state? : string;
 }
 
@@ -12,13 +12,15 @@ export interface GithubOAuthParams {
  * Github oauth scope 제한 파라미터
  * 프로젝트 내에선 GithubScope 타입으로 제한하여 사용하도록 권장합니다.
  */
-export interface StrictGithubOAuthParams  
-extends Omit<GithubOAuthParams, 'state'>,  Record<string, string> {
-    scope : GitHubScope;
+export interface StrictGithubOAuthParams  extends Omit<GithubOAuthParams, 'state'> {
+    scope : GitHubScope[]
 }
 // strict 타입이기 때문에 Record로 undefined가 불가능하게 강제시킴
-// state는 CSRF 공격 방지용으로 사용되지만, 현재 구현에서는 생략시켰음. 필요에 따라 추가할 수 있음
+// state는 CSRF 공격 방지용으로 사용되지만, 현재 구현에서는 생략시켰음. 
+// 필요에 따라 추가할 수 있음
 
 
-// github oauth scope 타입
+/** 
+ * github oauth scope 타입
+ */
 export type GitHubScope = 'user' | 'repo' | 'gist' | 'read:org';
