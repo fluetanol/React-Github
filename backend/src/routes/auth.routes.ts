bddeab34a91b0ea22f67bde17077c3cf6a00f689
Jung TaeSeung <mike415415@naver.com>
2026-05-01 00:49:00 +0900

    feat : backend github access token db 저장 로직 구현

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,8 @@
 import Routes, { CookieOptions } from 'express';
 import jwt from 'jsonwebtoken';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const auth_router = Routes();
 
@@ -34,11 +37,12 @@ auth_router.post('/github', async (req, res)=>{
 
         if(response.ok){
             const data =await response.json();
-            console.log("Github access token response:", data);
+        
+            console.log("Success : Github access token response:", data);
 
             // Github API를 사용하여 사용자 정보 요청
             const accessToken = data.access_token;
-            const userResponse = await fetch('https://api.github.com/user',
+            const githubUserResponse = await fetch('https://api.github.com/user',
                 {
                     headers : {
                         Authorization : `Bearer ${accessToken}`,
@@ -46,20 +50,59 @@ auth_router.post('/github', async (req, res)=>{
                 }
             )
 
-            const userData = await userResponse.json();
+            // 깃허브 사용자 정보
+            const githubUserData = await githubUserResponse.json();
+            console.log("Success : Github user data response:", githubUserData);
 
-            console.log("Github user data response:", userData);
+            const githubId = githubUserData.id;
+            const githubUsername = githubUserData.login;
+            const githubAccessToken = accessToken;
+
+            //테이블 명이 User라면 prisma 멤버에서 카멜케이스로 찾을 수 있음 
+            //upsert는 update + insert인 메서드로, 열이 존재하면 update, 존재하지 않으면 insert를 수행
+            const user = await prisma.user.upsert({
+                //github Id를 기준으로 검색 where githubId = githubId
+                where : {
+                    githubId : githubId,    
+                },
+                // 열 정보가 존재할 때 업데이트 할 내용, 쿼리로 치면 
+                // update User set githubUsername = githubUsername, githubAccessToken = githubAccessToken where githubId = githubId
+                update : {
+                    githubUsername : githubUsername,
+                    githubAccessToken : githubAccessToken,
+                },
+                // 열 정보가 존재하지 않을 때 생성할 내용, 쿼리로 치면
+                // insert into User (githubId, githubUsername, githubAccessToken) values (githubId, githubUsername, githubAccessToken)
+                create : {
+                    githubId : githubId,
+                    githubUsername : githubUsername,
+                    githubAccessToken : githubAccessToken,
+                }
+            });
+
+            console.log("Success : Update and Insert user data To DB", user);
+     
+            // model User{
+            //     id Int @id @default(autoincrement())
+            //     githubId Int @unique()
+            //     githubUsername String
+            //     githubAccessToken String
+            // }
 
             const jwtToken = process.env.JWT_SECRET;
-
             const appToken = jwt.sign(
                 {                           //payload
-                    userId : userData.id,
-                    email:userData.email,
+                    userId : user.id,
+                    githubId : user.githubId,
                 },
                  jwtToken!,                 //secret key
                 { expiresIn : '1h' }        //options
             );
+
+            //sameSite 옵션
+            // - 'strict' : 엄격한 sameSite 정책, 타 사이트에서 쿠키 전송 불가
+            // - 'lax' : 타 사이트에서 쿠키 전송 허용, 단 GET 요청에 한함
+            // - 'none' : 모든 상황에서 쿠키 전송 허용, 단 secure 옵션도 true로 설정해야 함 (브라우저 강제 사항)
 
             const cookieOptions : CookieOptions ={
                 httpOnly : true,        //http only 활성화
@@ -67,22 +110,19 @@ auth_router.post('/github', async (req, res)=>{
                 sameSite : 'strict',    //CSRF 공격 방지
                 maxAge : 3600000, //1시간
             }
-
-            //sameSite 옵션
-            // - 'strict' : 엄격한 sameSite 정책, 타 사이트에서 쿠키 전송 불가
-            // - 'lax' : 타 사이트에서 쿠키 전송 허용, 단 GET 요청에 한함
-            // - 'none' : 모든 상황에서 쿠키 전송 허용, 단 secure 옵션도 true로 설정해야 함 (브라우저 강제 사항)
-
             res.cookie('app_token', appToken, cookieOptions);
+    
+            console.log("Success : Set cookie with JWT token", { appToken, cookieOptions });
 
             res.json({
                 success :true,
-                user : userData,
+                user : githubUserData,
             })
         }
 
     }
     catch(error){
+        console.log("Error : Github authentication error", error);
          res.status(500).json({error : '인증 실패'});   
     }
 } )
