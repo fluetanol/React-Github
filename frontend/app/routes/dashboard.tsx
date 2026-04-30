06ccf241078fda690b396875c58496c4f1e38130
Jung TaeSeung <mike415415@naver.com>
2026-04-30 11:36:27 +0900

    feat : frontend dashboard 라우팅 로직 추가

---
+++ frontend/app/routes/dashboard.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Dashboard(){
+
+    return (
+        <div>
+            <div className ="flex flex-col items-center justify-center min-h-screen">
+            <h1>Dashboard</h1>
+            <p>대시보드 페이지입니다.</p>
+            </div>
+        </div>
+    )
+
+}
