81a9f2ccadb2f48c61e74ae5b9c299607c3fabb2
Jung TaeSeung <mike415415@naver.com>
2026-05-04 18:12:08 +0900

    feat : frontend dashboard 인증 실패 시 메인 화면 이동 로직

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
+import { useNavigate } from "react-router";
 
 
 
 export default function Dashboard(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [userDataState, setUserData] = useState<GithubUser>({} as GithubUser);
+    const navigate = useNavigate();
 
     useEffect(()=>{
-
         const fetchUserData = async()=>{
-
             try{
                 const res = await fetch("http://localhost:3000/api/users",
                     {
@@ -19,12 +21,16 @@ export default function Dashboard(){
                 if(res.ok){
                     const data = await res.json();
                     console.log("User data response:", data);
+                    setIsLoading(false);
+                    setUserData(data.user);
                 }
                 else{
                     throw new Error("Failed to fetch user data");
                 }
             }catch(error){
                 console.error("Error : Fetch user data error", error);
+                navigate('/'); //에러 발생 시 홈으로 리다이렉트
+                
             }
         }
 
@@ -37,8 +43,23 @@ export default function Dashboard(){
     return (
         <div>
             <div className ="flex flex-col items-center justify-center min-h-screen">
-            <h1>Dashboard</h1>
-            <p>대시보드 페이지입니다.</p>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div className="flex flex-col items-center gap-4">  
+
+                        <h1 className="text-2xl font-bold">Welcome, {userDataState.login}!</h1>
+                        <img src={userDataState.avatar_url} alt="Avatar" className="w-32 h-32 rounded-full" />
+                        <p>Name: {userDataState.name}</p>
+                        <p>Company: {userDataState.company || 'N/A'}</p>
+                        <p>Blog: {userDataState.blog || 'N/A'}</p>
+                        <p>Location: {userDataState.location || 'N/A'}</p>
+                        <p>Email: {userDataState.email || 'N/A'}</p>
+                        <p>Bio: {userDataState.bio || 'N/A'}</p> 
+
+
+                    </div>
+                )}
             </div>
         </div>
     )
