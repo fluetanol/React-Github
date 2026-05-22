a8479c2df0b6243fb7d1db3c35c23b30e667012b
Jung TaeSeung <mike415415@naver.com>
2026-05-23 02:12:34 +0900

    feat : frontend testpage 추가

--- frontend/app/components/common/Header.tsx
+++ frontend/app/components/common/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router"
 import { Github } from "~/icons/Github"
 
 export interface HeaderComponentProps{
@@ -11,6 +12,10 @@ export interface HeaderComponentProps{
 
 export default function Header({ userDataState }: HeaderComponentProps){
 
+    const navigate = useNavigate();
+    
+
+
     return(
         <header className="sticky top-0 z-10 bg-white/80 px-6 py-4 shadow-[0_12px_40px_rgba(15,23,42,0.08)] backdrop-blur-xl dark:bg-gray-950/70">
             <div className="mx-auto flex max-w-360 items-center justify-between">
@@ -22,6 +27,15 @@ export default function Header({ userDataState }: HeaderComponentProps){
                     </div>
                     <span className="text-sm font-semibold uppercase tracking-[0.24em] text-gray-500 dark:text-gray-400">Dashboard</span>
                 </div>
+
+                <div className="flex flex-row gap-3 text- font-medium tracking-[0.12em] text-gray-800 dark:text-gray-200">
+                    <button className="hover:text-gray-400 hover:cursor-pointer dark:hover:text-gray-300">Stastics</button>
+                    <button className="hover:text-gray-400 hover:cursor-pointer dark:hover:text-gray-300" 
+                    onClick={()=>{
+                        navigate("/testpage");
+                    }}>Test</button>
+                </div>
+
                 <div className="flex items-center gap-3 rounded-full bg-white px-3 py-2 shadow-[0_10px_30px_rgba(15,23,42,0.08)] dark:bg-gray-900">
                     <img
                         src={userDataState.avatar_url}
--- frontend/app/routes/testpage.tsx
+++ frontend/app/routes/testpage.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from "react"
+import { HTTPCredentials } from "~/types/utils/simpleFetcher/simpleFetcher";
+import { dench } from "~/utils/dench";
 
 
 
@@ -5,7 +8,44 @@
 
 export default function TestPage(){
 
+    const denchInstance = useRef(dench("http://localhost:3000/api/", "3000 test"));
+
+
+    useEffect(()=>{
+        const fetchTest = async () =>{
+            const dench = denchInstance.current;
+            try{
+
+
+                const responses = await Promise.all([
+                    //dench.get("testing/health").toResponse(),
+                    //dench.get("testing/authhealth").credentials(HTTPCredentials.INCLUDE).toResponse(),
+                    dench.post("testing/health", { message: "Hello, server!" })
+                    .sendJson()
+                    .toResponse(),
+                    dench.post("testing/authhealth", { message: "Hello, authenticated server!" })
+                    .sendJson()
+                    .credentials(HTTPCredentials.INCLUDE)
+                    .toResponse()
+                ])
+
+                console.log("Response from /api/testing/health:", responses[0]);
+                console.log("Response from /api/testing/authhealth:", responses[1]);
+
+
+            }catch(error){
+                console.error("Error fetching /api/testing/health:", error);
+             
+
+            }
+        }
+        fetchTest();
+    }, [])
+
+
     return(
-        <></>
+        <>
+            
+        </>
     )
 }
