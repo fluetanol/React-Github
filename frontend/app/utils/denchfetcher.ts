c4d72e4c9b0a8c16411e67676b32973c2ebe36de
Jung TaeSeung <mike415415@naver.com>
2026-05-26 17:14:21 +0900

    refactor : frontend denchfetcher 쓸데 없는 코드 정리

--- frontend/app/utils/denchfetcher.ts
+++ frontend/app/utils/denchfetcher.ts
@@ -1,35 +1,5 @@
 import type { DenchConfig } from "~/types/utils/simpleFetcher/dench";
 
-
-async function toFormData(res : Response) : Promise<FormData>{
-    if (res instanceof Response && res.ok){
-            return res.formData();
-    }
-    else{
-        throw new Error(`API request failed: ${res}`);
-    }
-}
-
-async function toJsonData<T>(res: Response): Promise<T> {
-    if (res instanceof Response && res.ok) {
-        return res.json() as unknown as T;
-    }
-    else {
-        throw new Error(`API request failed: ${res}`);
-    }
-}
-
-
-async function toObject<T>(res: Response): Promise<T> {
-    if (res instanceof Response && res.ok) {
-        return res as unknown as T;
-    }
-    else {
-        throw new Error(`API request failed: ${res}`);
-    }
-}
-
-
 export default async function denchfetcher<T>(url: string, config?: DenchConfig) :  Promise<Response> {
 
     let res : Response;
