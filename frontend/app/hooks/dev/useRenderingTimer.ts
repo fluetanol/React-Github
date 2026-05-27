45dc54ba65f63cc9df8bd1fb9aef14f8d67282fb
Jung TaeSeung <mike415415@naver.com>
2026-05-28 01:34:48 +0900

    feat : frontend rendering timer+regist loading 추가

--- frontend/app/hooks/dev/useRenderingTimer.ts
+++ frontend/app/hooks/dev/useRenderingTimer.ts
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useGlobalLoadingMap } from "./useRegistLoading";
 
 /**
  * 렌더링 시간을 측정해주는 커스텀 훅입니다.
@@ -6,26 +7,28 @@ import { useEffect, useRef } from "react";
  * @param label 타이머 라벨로, 콘솔에 출력될 때 어떤 타이머인지 식별할 수 있도록 도와줍니다.
  * @param isLoading 데이터 로딩 상태를 나타내는 상태 값이며, 이 값이 false로 변경될 때 렌더링 시간을 측정하여 콘솔에 출력합니다.
  */
-export default function useRenderingTimer(label : string, isLoading : boolean) : number{
-    const startTime = useRef(performance.now());
-    const duration = useRef<number>(0);
-    useEffect(()=>{
-        if(!isLoading){
-            const endTime = performance.now();
+export default function useRenderingTimer(label : string) : number{
 
-            duration.current = endTime - startTime.current;
+    const loadingMap = useGlobalLoadingMap();
+    const isAnyLoading = Object.values(loadingMap).some(Boolean);
+    const startTime = useRef(performance.now());
+    const [duration, setDuration] = useState<number>(0);
+    
+    useEffect(()=>{
+        if(!isAnyLoading){
+            const endTime = performance.now();
+            const result = endTime - startTime.current;
+            setDuration(result);
 
             console.log(
-                `%c[Timer: ${label}] %c${duration.current.toFixed(2)}ms`,
+                `%c[Timer: ${label}] %c${result.toFixed(2)}ms`,
                 "color: #2ecc71; font-weight: bold;", // 초록색 라벨
                 "color: #f1c40f; font-weight: bold;"  // 노란색 결과값
             );
         }
-    }, [isLoading]);
+    }, [isAnyLoading, label]);
 
-    if(!isLoading){
-        return duration.current;
-    }
+    console.log(`useRenderingTimer - startTime = ${startTime.current.toFixed(2)}ms, duration = ${duration.toFixed(2)}ms`);
 
-    return -1;
+    return duration;
 }
